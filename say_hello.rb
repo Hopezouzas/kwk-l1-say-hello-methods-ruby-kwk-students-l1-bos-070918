17ec1724365b88b98a18efe72b5e7a3627893cf6
Hope Zouzas <hmzouzas@gmail.com>
2018-07-10 15:45:05 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
- def say_hello(name = "ruby_programmer")
-   puts "Hello #{name}"
+ def say_hello(your_name = "ruby_programmer")
+   puts "Hello #{your_name}"
 end
 say_hello
 
